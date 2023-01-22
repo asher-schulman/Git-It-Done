9078362a2d70a6f577300650f7090fdc7f2d09a6
Asher Schulman <asher.schulman@gmail.com>
2023-01-22 08:53:33 -0600

    refactor API calls from Heroku url -> Cyclic url

--- src/components/Workspace/Workspaces.js
+++ src/components/Workspace/Workspaces.js
@@ -1,109 +1,112 @@
 import React, { useState, useEffect } from "react";
 import WorkspaceForm from "./WorkspaceForm";
 import Workspace from "./Workspace";
-import './Workspace.css'
-import { Spin } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
+import "./Workspace.css";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const Workspaces = () => {
-  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
-  
-  const [workspaces, setWorkspaces] = useState("");
+	const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
-  const [showCreate, setShowCreate] = useState(false);
+	const [workspaces, setWorkspaces] = useState("");
 
-  const onToggle = () => {
-    setShowCreate(!showCreate);
-  };
+	const [showCreate, setShowCreate] = useState(false);
 
-  const URL = "https://git-it-done-backend.herokuapp.com/api/workspaces/";
+	const onToggle = () => {
+		setShowCreate(!showCreate);
+	};
 
-  const getWorkspaces = async () => {
-    const response = await fetch(URL);
-    const data = await response.json();
-    setWorkspaces(data);
-  };
+	// PROD
+	const URL = "https://git-it-done-backend.cyclic.app/api/api/workspaces/";
+	// DEV
+	// const URL = "http://localhost:8000/api/workspaces/";
 
-  const createWorkspace = async (workspaces) => {
-    await fetch(URL, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(workspaces),
-    });
-    getWorkspaces();
-  };
+	const getWorkspaces = async () => {
+		const response = await fetch(URL);
+		const data = await response.json();
+		setWorkspaces(data);
+	};
 
-  const updateWorkspace = async (workspace, id) => {
-    await fetch(URL + id, {
-      method: "put",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(workspace),
-    });
-    getWorkspaces();
-  };
+	const createWorkspace = async (workspaces) => {
+		await fetch(URL, {
+			method: "post",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(workspaces),
+		});
+		getWorkspaces();
+	};
 
-  const deleteWorkspace = async (id) => {
-    await fetch(URL + id, {
-      method: "delete",
-    });
-    getWorkspaces();
-  };
-  useEffect(() => getWorkspaces(), []);
+	const updateWorkspace = async (workspace, id) => {
+		await fetch(URL + id, {
+			method: "put",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(workspace),
+		});
+		getWorkspaces();
+	};
 
-  // const loaded = () => {
-  //   const userId = JSON.parse(localStorage.getItem('user')).googleId
-  //   return workspaces.map((workspace, index) => {
-  //     return workspace.userId.map((workspaceId, index) => {
-  //       console.log(` workid ${workspaceId}`)
-  //       console.log(` userid ${userId}`)
-  //       if (workspaceId === userId)
-  //         return <Workspace key={index} workspace={workspace} updateWorkspace={updateWorkspace} deleteWorkspace={deleteWorkspace} />
-  //     })
-  //     })
-  // }
+	const deleteWorkspace = async (id) => {
+		await fetch(URL + id, {
+			method: "delete",
+		});
+		getWorkspaces();
+	};
+	useEffect(() => getWorkspaces(), []);
 
-  const loaded = () => {
-    const userId = JSON.parse(localStorage.getItem("user")).googleId;
-    const spaces = [];
-    workspaces.forEach((workspace, index) => {
-      if (workspace.userId.includes(userId)) {
-        spaces.push(
-          <Workspace
-            key={index}
-            index={index}
-            workspace={workspace}
-            updateWorkspace={updateWorkspace}
-            deleteWorkspace={deleteWorkspace}
-          />
-        );
-      }
-    });
-    return spaces;
-  };
+	// const loaded = () => {
+	//   const userId = JSON.parse(localStorage.getItem('user')).googleId
+	//   return workspaces.map((workspace, index) => {
+	//     return workspace.userId.map((workspaceId, index) => {
+	//       console.log(` workid ${workspaceId}`)
+	//       console.log(` userid ${userId}`)
+	//       if (workspaceId === userId)
+	//         return <Workspace key={index} workspace={workspace} updateWorkspace={updateWorkspace} deleteWorkspace={deleteWorkspace} />
+	//     })
+	//     })
+	// }
 
-  const loading = () => {
-    return <Spin indicator={antIcon} />
-  };
+	const loaded = () => {
+		const userId = JSON.parse(localStorage.getItem("user")).googleId;
+		const spaces = [];
+		workspaces.forEach((workspace, index) => {
+			if (workspace.userId.includes(userId)) {
+				spaces.push(
+					<Workspace
+						key={index}
+						index={index}
+						workspace={workspace}
+						updateWorkspace={updateWorkspace}
+						deleteWorkspace={deleteWorkspace}
+					/>
+				);
+			}
+		});
+		return spaces;
+	};
 
-  return (
-    <>
-      <div className='create-btn' onClick={onToggle}>
-        Create Workspaces
-      </div>
-      {showCreate && (
-        <WorkspaceForm
-          createWorkspace={createWorkspace}
-          showCreate={showCreate}
-          setShowCreate={setShowCreate}
-        />
-      )}
-      {workspaces ? loaded() : loading()}
-    </>
-  );
+	const loading = () => {
+		return <Spin indicator={antIcon} />;
+	};
+
+	return (
+		<>
+			<div className="create-btn" onClick={onToggle}>
+				Create Workspaces
+			</div>
+			{showCreate && (
+				<WorkspaceForm
+					createWorkspace={createWorkspace}
+					showCreate={showCreate}
+					setShowCreate={setShowCreate}
+				/>
+			)}
+			{workspaces ? loaded() : loading()}
+		</>
+	);
 };
 
 export default Workspaces;
